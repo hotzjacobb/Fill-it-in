f548603e31ceaa4a113c1abbe7a621ba61ed3640
hotzjacobb <hotzjacobb@gmail.com>
2020-01-02 07:18:56 -0800

    main screen in place

--- UpForIt/ViewController.swift
+++ UpForIt/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // TODO: check if the user's first time to offer tutorial
+        
+        // TODO: if not logged in create alert asking user if they want to play as guest or not. Default behavior is to save user's profile data and be logged in from the get-go.
     }
 
 
