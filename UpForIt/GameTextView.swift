d66d6b11542d9a939cbe6cf3b5c5075231996337
hotzjacobb <hotzjacobb@gmail.com>
2020-02-07 09:31:33 -0800

    WikipediaKit dependency added with Carthage

---
+++ UpForIt/GameTextView.swift
@@ -0,0 +1,22 @@
+//
+//  GameTextView.swift
+//  UpForIt
+//
+//  Created by Jeffrey Hotz on 2020-01-02.
+//  Copyright © 2020 Jacob Hotz. All rights reserved.
+//
+
+import UIKit
+
+class GameTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
---
+++ UpForIt/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  UpForIt
+//
+//  Created by Jeffrey Hotz on 2020-01-02.
+//  Copyright © 2020 Jacob Hotz. All rights reserved.
+//
+
+import UIKit
+import WikipediaKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Fetch wikipedia article
+        //fetchWikiArticle()
+    }
+    
+    
+    
+//    func fetchWikiArticle(completion: @escaping(Result<WeatherDataTemp, Error>) -> Void) {
+//    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
